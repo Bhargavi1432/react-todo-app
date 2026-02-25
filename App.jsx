8713049b4bb11b53afc0501450d498b4ca36fb56
Bhargavi Jakki <120722292+Bhargavi1432@users.noreply.github.com>
2026-02-25 14:37:37 +0530

    App

--- App.jsx
+++ App.jsx
@@ -1,13 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/Profile" element={<Profile />} />
       </Routes>
-    </BrowserRouter>
   );
 }
 
