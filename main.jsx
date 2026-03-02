1b25fd1018bd6e80898886e8a30ffbead89ecff4
Bhargavi Jakki <120722292+Bhargavi1432@users.noreply.github.com>
2026-03-02 11:09:22 +0530

    main

--- main.jsx
+++ main.jsx
@@ -7,3 +7,18 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </BrowserRouter>
 );
+
+
+//
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
+
+
