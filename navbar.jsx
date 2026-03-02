004d690494ce45df90799129907774ccf4a0c675
Bhargavi Jakki <120722292+Bhargavi1432@users.noreply.github.com>
2026-03-02 12:37:51 +0530

    navbar

--- navbar.jsx
+++ navbar.jsx
@@ -1,11 +1,11 @@
-
 import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav style={{ padding: "15px", background: "#eee" }}>
       <Link to="/" style={{ marginRight: "15px" }}>Dashboard</Link>
-      <Link to="/profile">Profile</Link>
+      <Link to="/profile" style={{ marginRight: "15px" }}>Profile</Link>
+      <Link to="/users">Users</Link>
     </nav>
   );
 }
