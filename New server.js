4338cb3c7709184d058a00af9a2e49e68ef6da24
Bhargavi Jakki <120722292+Bhargavi1432@users.noreply.github.com>
2026-03-24 12:55:46 +0530

    Update New server.js

--- New server.js
+++ New server.js
@@ -6,19 +6,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API running...");
-});
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-
-
-
-//connect Routes 
+// ✅ Correct paths
 const userRoutes = require("./routes/userRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
