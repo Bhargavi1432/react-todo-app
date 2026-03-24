d67b2ab1707971ba43a58aa9e4ca5dde126a8634
Bhargavi Jakki <120722292+Bhargavi1432@users.noreply.github.com>
2026-03-24 15:46:14 +0530

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,17 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Login Page */}
-        <Route path="/" element={<Login />} />
-
-        {/* Temporary Dashboard */}
-        <Route path="/dashboard" element={<h1>Dashboard Page</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<h1>Dashboard Page</h1>} />
+    </Routes>
   );
 }
 
