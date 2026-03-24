3b8c9bc3d2dac93bf00ebab942de3064ca44ede1
Bhargavi Jakki <120722292+Bhargavi1432@users.noreply.github.com>
2026-03-24 23:12:44 +0530

    Delete navbar.jsx

--- navbar.jsx
+++
@@ -1,9 +0,0 @@
-export default function Navbar({ setCategory }) {
-  return (
-    <div>
-      <button onClick={() => setCategory("work")}>Work</button>
-      <button onClick={() => setCategory("study")}>Study</button>
-      <button onClick={() => setCategory("personal")}>Personal</button>
-    </div>
-  );
-}
