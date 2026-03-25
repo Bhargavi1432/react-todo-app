9789583d7b8ccda8af0286a14e0f9d66e6c8d4aa
Bhargavi Jakki <120722292+Bhargavi1432@users.noreply.github.com>
2026-03-25 09:28:06 +0530

    Update register.jsx

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,1 +1,30 @@
+import { useState } from "react";
+import API from "../services/api";
+import { useNavigate } from "react-router-dom";
 
+export default function Register() {
+  const [user, setUser] = useState({ username: "", password: "" });
+  const navigate = useNavigate();
+
+  const handleRegister = async () => {
+    await API.post("/register", user);
+    alert("Registered successfully ✅");
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+
+      <input placeholder="Username"
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+      />
+
+      <input placeholder="Password" type="password"
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
+}
